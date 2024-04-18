244dbc6c76273bb50ea23c509a96ea23527a9736
Beatriz Araujo <beatriz.a.0702@gmail.com>
2024-04-18 11:46:05 -0300

    chore: cria campos adicionais na pagina detalhada

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -48,38 +48,71 @@ export default async function DetailedCharacterPage({params: {id}}: DetailedChar
     type CharacterFields = Pick<Character, "name" | "height" | "mass" | "gender"| "eyeColor" | "hairColor" | "skinColor" | "birthYear">;
 
     const fields: CharacterFields = {
-        name: 'Name',
+        name: "Name",
         height: "Height",
         mass: "Weight",
         gender: "Gender",
         eyeColor: "Eye color",
         hairColor: "Hair color",
         skinColor: "Skin color",
-        birthYear: 'Year of birth',
+        birthYear: "Year of birth",
     }
 
     const homeworldFields = {
-        name: 'Name',
-        rotation_period: 'Rotation period',
-        orbital_period: 'Orbital period',
-        diameter: 'Diameter',
-        climate: 'Climate',
-        gravity: 'Gravity',
-        terrain: 'Terrain',
-        surface_water: 'Surface water',
-        population: 'Population',
+        name: "Name",
+        rotation_period: "Rotation period",
+        orbital_period: "Orbital period",
+        diameter: "Diameter",
+        climate: "Climate",
+        gravity: "Gravity",
+        terrain: "Terrain",
+        surface_water: "Surface water",
+        population: "Population",
     }
 
     const filmFields = {
-
+        title: "Title",
+        episode_id: "Episode",
+        director: "Director",
+        producer: "Producer",
+        release_date: "Release date"
     }
 
-    const VehicleFields = {
-
+    const vehicleFields = {
+        name: "Name",
+        model: "Model",
+        manufacturer: "Manufacturer",
+        cost_in_credits: "Cost",
+        length: "Length",
+        max_atmospheric_speed: "Max atmospheric speed",
+        cargo_capacity: "Cargo capacity",
+        consumables: "Consumables",
+        vehicle_class: "Vehicle class"
     }
 
     const starshipFields = {
-        
+        name: "Name",
+        model: "Model",
+        manufacturer: "Manufacturer",
+        cost_in_credits: "Cost",
+        length: "Length",
+        max_atmospheric_speed: "Max atmospheric speed",
+        cargo_capacity: "Cargo capacity",
+        consumables: "Consumables",
+        hyperdrive_rating: 'Hyperdrive rating',
+        MGLT: "MGLT",
+        starship_class: "Vehicle class",
+    }
+
+    function createRowsOfData(typeOfField: string, fieldsObject: Record<string, string>, valueObject: any){
+        return Object.entries(fieldsObject).map(([key, value]) => {
+            return (
+                <article key={`${typeOfField}-${key}`}>
+                    <p>{value}</p>
+                    <p>{valueObject[key]}</p>
+                </article>
+            )
+        })
     }
 
     return character instanceof CustomError? (
@@ -90,32 +123,27 @@ export default async function DetailedCharacterPage({params: {id}}: DetailedChar
         <main>
             <div>
                 <h3>Character data</h3>
-                {
-                    Object.entries(fields).map(([key, value]) => {
-                        return (
-                            <article key={`field-${key}`}>
-                                <p>{value}</p>
-                                <p>{character[key as keyof CharacterFields]}</p>
-                            </article>
-                        )
-                    })
-                }
+                { createRowsOfData('character-fields', fields, character) }
             </div>
 
             <div>
                 <h3>Homeworld data</h3>
+                { createRowsOfData('character-fields', homeworldFields, character.homeworldData) }
             </div>
 
             <div>
                 <h3>Films data</h3>
+                { character.filmsData!.map(film => createRowsOfData('character-fields', filmFields, film))}
             </div>
 
             <div>
                 <h3>Vehicles data</h3>
+                { character.vehiclesData!.map(vehicle => createRowsOfData('character-fields', vehicleFields, vehicle))}
             </div>
 
             <div>
                 <h3>Starships data</h3>
+                { character.starshipsData!.map(starship => createRowsOfData('character-fields', starshipFields, starship))}
             </div>
         </main>
     )
