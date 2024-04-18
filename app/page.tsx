bca49f8232291c69f63790f68714cef3e1887959
Beatriz Araujo <beatriz.a.0702@gmail.com>
2024-04-18 00:31:09 -0300

    chore: adiciona paginação com limites max e min de paginas

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,95 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useMemo, useState } from "react";
 import styles from "./page.module.css";
+import { ApiResponse, Character } from "./libs/types";
+import CharacterCard from "./components/characterCard";
 
 export default function Home() {
+  const [page, setPage] = useState(1);
+  const isPageMin = useMemo(() => {
+    return page === 1;
+  }, [page]);
+
+  function increasePage() {
+    if (isPageMax) return;
+    setPage((previousPage) => previousPage + 1);
+  }
+
+  function decreasePage() {
+    if (isPageMin) return;
+    setPage((previousPage) => previousPage - 1);
+  }
+
+  const [results, setResults] = useState<Character[] | null>(null);
+  const [count, setCount] = useState(0);
+
+  const maxPage = useMemo(() => {
+    const hasAdditionalPage = count % 10 > 0;
+    return Math.round(count / 10) + (hasAdditionalPage ? 1 : 0);
+  }, [count]);
+
+  const isPageMax = useMemo(() => {
+    return page === maxPage;
+  }, [maxPage, page]);
+
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function fetchCharacters() {
+    try {
+      setError(false);
+      const response = await fetch(
+        `https://swapi.dev/api/people/?page=${page}`,
+        { cache: "no-store" }
+      );
+      const data: ApiResponse = await response.json();
+      setResults(data.results.map(el =>  new Character(el)));
+      setCount(data.count);
+      return;
+    } catch (e) {
+      setError(true);
+      setPage(1);
+      setCount(0);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetchCharacters();
+  }, [page]);
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      <p>
+        {page} / {maxPage}
+      </p>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {isLoading && <p>Loading</p>}
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <section className={styles.cardsGrid}>
+        {results === null ? (
+          <p>aaa</p>
+        ) : (
+          results.map((el, i) => {
+            return (
+              <CharacterCard
+                height={el.height}
+                mass={el.mass}
+                gender={el.gender}
+                name={el.name}
+                id={el.id}
+                key={`character-${el.id}-${el.name}`}
+              />
+            );
+          })
+        )}
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <button onClick={decreasePage}>previous</button>
+      <button onClick={increasePage}>next</button>
     </main>
   );
 }
